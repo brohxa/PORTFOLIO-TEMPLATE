28aaf09ae01dc0d029ac29664ced1385438ecdcc
brohxa <64731231+brohxa@users.noreply.github.com>
2020-05-23 15:15:23 +0530

    done PORT FOLIO

--- portfolio/app.js
+++ portfolio/app.js
@@ -27,7 +27,7 @@ const tl2 = new TimelineMax();
 tl.fromTo(right, 2, { width: "30%" }, { width: "50%", ease: Power2.easeInOut }).fromTo(nameText, 1, { y: "-50", opacity: 0 }, { y: "0", opacity: 1 });
 tl2.fromTo(bigText, 2, { y: "-50", opacity: 0 }, { y: "0", opacity: 1 }).fromTo(graybox, 2, { width: "0px", opacity: 0 }, { width: "250px", opacity: 1 });
 
-// SCROLL HAMBUREGER
+// SCROLL COVER
 
 const cover = document.querySelector('.cover');
 
@@ -36,3 +36,32 @@ window.addEventListener('scroll', function () {
         cover.classList.add('active');
     }
 })
+
+// COLOR CHANGE FOR BURGER AND SIDEBAR
+
+const lines = document.querySelectorAll('.line')
+const links = document.querySelectorAll('.link')
+
+for (let i = 0; i < lines.length; i++) {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 900 & window.scrollY < 1800) {
+            lines[i].classList.add('active')
+        }
+        else {
+            lines[i].classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', function () {
+    for (let i = 0; i < links.length; i++) {
+        if (window.scrollY > 700 & window.scrollY < 1800) {
+            links[i].classList.add('active')
+        }
+        else {
+            links[i].classList.remove('active')
+        }
+    }
+})
+
+
