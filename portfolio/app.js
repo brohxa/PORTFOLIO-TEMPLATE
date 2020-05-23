f3654f774aab99d39b61d74aacd703b822a0363c
brohxa <64731231+brohxa@users.noreply.github.com>
2020-05-23 20:32:00 +0530

    START BLOG PAGE

--- portfolio/app.js
+++ portfolio/app.js
@@ -25,6 +25,7 @@ const tl = new TimelineMax();
 const tl2 = new TimelineMax();
 
 tl.fromTo(right, 2, { width: "30%" }, { width: "50%", ease: Power2.easeInOut }).fromTo(nameText, 1, { y: "-50", opacity: 0 }, { y: "0", opacity: 1 });
+
 tl2.fromTo(bigText, 2, { y: "-50", opacity: 0 }, { y: "0", opacity: 1 }).fromTo(graybox, 2, { width: "0px", opacity: 0 }, { width: "250px", opacity: 1 });
 
 // SCROLL COVER
@@ -44,7 +45,7 @@ const links = document.querySelectorAll('.link')
 
 for (let i = 0; i < lines.length; i++) {
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 900 & window.scrollY < 1800) {
+        if (window.scrollY > 900 & window.scrollY < 1820) {
             lines[i].classList.add('active')
         }
         else {
@@ -55,7 +56,7 @@ for (let i = 0; i < lines.length; i++) {
 
 window.addEventListener('scroll', function () {
     for (let i = 0; i < links.length; i++) {
-        if (window.scrollY > 700 & window.scrollY < 1800) {
+        if (window.scrollY > 750 & window.scrollY < 1750) {
             links[i].classList.add('active')
         }
         else {
