737c0d6929edb3707134bbb2cf0fb95d457dd77d
brohxa <64731231+brohxa@users.noreply.github.com>
2020-05-29 00:15:59 +0530

    DONE CONNECTING

---
+++ blog/Blog Page3/app.js
@@ -0,0 +1,31 @@
+let hamburger = document.querySelector('.hamburger')
+let sidebar = document.querySelector('.sidebar')
+let links = document.querySelectorAll('.link');
+let line1 = document.querySelector('.line1')
+let line2 = document.querySelector('.line2')
+let line3 = document.querySelector('.line3')
+
+let menuOpen = false;
+hamburger.addEventListener('click', () => {
+    if (!menuOpen) {
+        sidebar.style.animation = 'sidebar .5s forwards '
+        line1.style.animation = 'line1cross .5s forwards '
+        line2.style.animation = 'line2cross .5s forwards '
+        line3.style.animation = 'line3cross .5s forwards '
+        menuOpen = true;
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.animation = 'links 1s forwards '
+        }
+    }
+    else {
+        sidebar.style.animation = 'sidebarback .5s forwards'
+        line1.style.animation = 'line1crossback .3s forwards '
+        line2.style.animation = 'line2crossback .1s forwards '
+        line3.style.animation = 'line3crossback .3s forwards '
+        menuOpen = false;
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.animation = 'linksback 1s forwards '
+        }
+    }
+
+})
